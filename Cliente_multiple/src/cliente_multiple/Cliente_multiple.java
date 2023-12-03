9b0719b516db36670b596c805e9aea5317f668d8
alebandr0 <54161455+alebandr0@users.noreply.github.com>
2023-12-02 19:45:26 -0700

    Add files via upload

---
+++ Cliente_multiple/src/cliente_multiple/Cliente_multiple.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente_multiple;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Amit  Gyawali
+ */
+public class Cliente_multiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("192.168.56.1",5000)){
+            BufferedReader input = new BufferedReader( new
+                InputStreamReader(socket.getInputStream()));
+            PrintWriter output =
+                    new PrintWriter(socket.getOutputStream(),true);
+            Scanner scanner = new Scanner(System.in);
+            String userInput;
+            String response;
+            String clientName = "anonymous";
+            ClientThread clientThread = new ClientThread(socket);
+            clientThread.start();
+            do {
+                if (clientName.equals("anonymous")) {
+                    System.out.print("Escribe tu nombre>:");
+                    userInput = scanner.nextLine();
+                    clientName = userInput;
+                    output.println(userInput);
+                    if (userInput.equals("salir")) {
+                        break;
+                    }
+               } else {
+                    String message = ( "("+clientName+")"+" mensaje: " );
+                    System.out.print(message);
+                    userInput = scanner.nextLine();
+                    //output.println(message + " " + userInput);
+                    output.println(userInput);
+                    if (userInput.equals("salir")) {
+                        break;
+                    }
+                }
+            } while (!userInput.equals("salir"));
+        } catch (Exception e) {
+            System.out.println("Main client. Error:"+e.getMessage());
+        }
+    }
+}
